954420b930cad4e3ae3610c7c4b6f52407f35e10
Christine Zosche <christine.zosche@gmail.com>
2020-04-24 16:22:17 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -52,7 +52,7 @@ def sum_array(array)
 end
 
 def add_s(array)
-  array.each_with_index do |word, index|
+  array.each_with_index.collect do |word, index|
     if word != array[1]
       word + "s"
     end
